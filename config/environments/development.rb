24aca1f24b609d04b3eac2971c7eb31630e769e2
sevvett <ymgc2003rm@me.com>
2014-09-27 18:08:02 +0900

    Initial commi

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+Demo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
